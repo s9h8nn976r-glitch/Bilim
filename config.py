e1b2662ca993aa08bf2e381c71dd714525d709cf
s9h8nn976r-glitch <s9h8nn976r@privaterelay.appleid.com>
2026-08-30 19:31:47 +0500

    Add environment variable keys for API tokens

--- config.py
+++ config.py
@@ -2,4 +2,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
