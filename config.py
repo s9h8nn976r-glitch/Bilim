641d65d81d4b1455b3ce6a72b88219e29748d826
s9h8nn976r-glitch <s9h8nn976r@privaterelay.appleid.com>
2026-08-31 03:12:19 +0500

    chore: add search and mathpix keys to config

--- config.py
+++ config.py
@@ -3,3 +3,6 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY", "")
 OPENROUTER_API_KEY = os.environ.get("OPENROUTER_API_KEY", "")
+BING_SEARCH_KEY = os.environ.get("BING_SEARCH_KEY", "")
+MATHPIX_KEY = os.environ.get("MATHPIX_KEY", "")
+MATHPIX_APP_ID = os.environ.get("MATHPIX_APP_ID", "")
